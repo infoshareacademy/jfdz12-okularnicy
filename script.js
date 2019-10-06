500b6404b472a9d2375ca0df7ee3ab83340d4138
Helena Makrenek <hmakrenek@gmail.com>
2019-10-06 19:49:08 +0200

    FZ12OK-11 cookies compleated

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ $(function () {
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
 });
+
+// cookieInfo
+
+const cookieInfo = document.querySelector(".cookie");
+const cookieYesBtn = document.querySelector(".cookie-btn-yes");
+const cookieNoBtn = document.querySelector(".cookie-btn-no");
+
+
+
+const cookieShow = () => {
+  if (!localStorage.cookie) {
+    cookieInfo.style.display = "block";
+
+    cookieYesBtn.addEventListener("click", () => {
+      localStorage.setItem("cookie", "cookie");
+      cookieInfo.style.display = "none";
+    })
+
+    cookieNoBtn.addEventListener("click", () => {
+      alert("Sorry! You need to fly away!")
+      window.open("https://www.youtube.com/watch?v=EvuL5jyCHOw#t=0m54s");
+    })
+  }
+};
+
+cookieShow();
+
+// end of cookieInfo
