144ef54168282791589d78a68b91c067d84e8db9
Kamil Jaworski <jaworskiebiznes@gmail.com>
2019-10-26 20:07:49 +0200

    Test1

--- game2/game2.js
+++ game2/game2.js
@@ -20,6 +20,40 @@ addEventListener("keydown", function (event) {
     }
 });
 
+const fire = document.querySelector('.game-window_fire');
+let alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+let first_alpha = 'a';
+
+function movingFire(arrayy) {
+    
+setTimeout(function() {
+    fire.style.gridArea = `${arrayy[1]}3`;
+        }, 1000);
+
+setTimeout(function() {
+    fire.style.gridArea = `${arrayy[2]}3`;
+        }, 2000);
+
+        setTimeout(function() {
+            fire.style.gridArea = `${arrayy[3]}3`;
+                }, 3000);
+                setTimeout(function() {
+                    fire.style.gridArea = `${arrayy[4]}3`;
+                        }, 4000);
+                        setTimeout(function() {
+                            fire.style.gridArea = `${arrayy[5]}3`;
+                                }, 5000);
+                                setTimeout(function() {
+                                    fire.style.gridArea = `${arrayy[6]}3`;
+                                        }, 6000);
+
+}
+movingFire(alpha);
+
+for (var i = 0; i < 10; i++){
+    console.log(i);
+}
+
 // addEventListener("keydown", function (event) {
 
 //     if (event.keyCode === 39) {
