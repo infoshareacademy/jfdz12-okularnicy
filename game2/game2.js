4493277f8d58cf48b55bc153609107bf80d8b020
Adam Brudzynski <adam.brudzynski@gmail.com>
2019-11-10 20:44:13 +0100

    FZ12OK-12-Game-II point system and timer added

--- game2/game2.js
+++ game2/game2.js
@@ -1,10 +1,62 @@
 const dangerousObjects = ["🔥", "🦇", "☁️", "🌧️", "🌪️", "❄️", "💣"]
 const bonusObjects = ["⛽"]
-const speed = 6;
+let speed = 6;
 let play = true;
-let time = 45000;
-let life = 100;
+let life = 100; //zacągnąć punkty z loc. storage z 1 etapu
 let points = 0;
+let left = 250;
+let score = 45
+let distance = 0
+
+const time = document.getElementById("time");
+const pointIndicator = document.querySelector(".points");
+const lifeIndicator = document.querySelector(".life");
+const plane = document.querySelector(".game-window__plane");
+
+function timer(callback, value) {
+    value = value || 45;
+    let timer = setInterval(function () {
+        callback(value);
+        if (value-- <= 0) {
+            setTimeout(gameEnd, 500);
+            clearInterval(timer);
+        }
+    }, 1000);
+};
+
+new timer(function (value) {
+    let timerMsg = "00:" + (value >= 10 ? value : "0" + value);
+    time.textContent = timerMsg;
+    pointIndicator.innerText = points;
+    points = Math.floor(life * distance * 0.0001);
+    score = value;
+});
+
+function gameOver() {
+    console.log("game over");
+    play = false;
+}
+
+function gameEnd() {
+    play = false;
+
+
+}
+
+function calculateDistance() {
+    distance += speed * score
+
+}
+
+function lifeLeft() {
+    if (life > 0) {
+        life -= 1
+        lifeIndicator.innerText = life;
+    } else if (life <= 0) {
+        gameOver()
+    }
+}
+
 
 class Obstacle {
     constructor(emoji) {
@@ -27,11 +79,12 @@ class Obstacle {
         let num = 0;
         const x = this.HTMLtag;
         const y = this.positon;
-        const interval = setInterval(function() {
+        const interval = setInterval(function () {
             num++;
             x.style.top = `${num * speed}px`;
             manageCollision()
-            if ((num * speed) > 600) {
+            calculateDistance()
+            if ((num * speed) > 650) {
                 stopMoving()
             }
         }, 100);
@@ -47,18 +100,20 @@ class Obstacle {
 
             const planePosiotionLeft = parseInt(plane.style.left.replace("px", ""))
 
-            if (planePosiotionLeft >= (objPosiitionLeft - 65) && planePosiotionLeft <= (objPosiitionLeft + 65) && (num * speed) > 520 && (num * speed) < 600) {
+            if (planePosiotionLeft >= (objPosiitionLeft - 65) 
+            && planePosiotionLeft <= (objPosiitionLeft + 65) 
+            && (num * speed) > 520 
+            && (num * speed) < 600) {
                 console.log("collision");
                 lifeLeft();
                 plane.animate([
                     { background: 'none' },
                     { background: 'red' }
                 ], {
-                    duration: 250,
-                    iterations: 1
-                });
+                        duration: 250,
+                        iterations: 1
+                    });
             }
-
 
         }
 
@@ -66,21 +121,17 @@ class Obstacle {
 
 }
 
-
-for (let i = 0; i < 2; i++) {
-    setInterval(function() {
+for (let i = 0; i < 3; i++) {
+    setInterval(function () {
         if (play) {
             const obj = new Obstacle();
             obj.generateObstacle()
             obj.moveObstacle()
         }
-    }, 3000);
+    }, 2000);
 }
 
-
-const plane = document.querySelector(".game-window__plane");
-let left = 250;
-addEventListener("keydown", function(event) {
+addEventListener("keydown", function (event) {
     if (event.keyCode === 37 && left > 1) {
         left -= 15;
         plane.style.left = `${left}px`;
@@ -88,26 +139,11 @@ addEventListener("keydown", function(event) {
     } else if (event.keyCode === 39 && left < 600) {
         left += 15;
         plane.style.left = `${left}px`;
+    } else if (event.keyCode === 38 && speed < 17) {
+        speed += 0.5;
+    } else if (event.keyCode === 40 && speed > 6) {
+        speed -= 0.5;
     }
 });
 
 
-const lifeIndicator = document.querySelector(".life");
-
-function lifeLeft() {
-    if (life > 0) {
-        life -= 0.5
-        lifeIndicator.innerHTML = life;
-    } else if (life <= 0) {
-        gameOver()
-    }
-}
-
-// function timer() {
-//     setTimeout(function)
-// }
-
-function gameOver() {
-    console.log("game over");
-    play = false;
-}
