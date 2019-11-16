5047a9b7495a014f6f3a61d7f5e8bd7dffd6b4a1
Adam <adam.brudzynski@gmail.com>
2019-11-16 13:31:31 +0100

    FZ2OK-12 game II minor fixes

--- game2.js
+++ game2.js
@@ -1,4 +1,4 @@
-const dangerousObjects = ["🔥", "🦆", "🦇", "🌧️", "🌪️", "❄️", "💩", "🌩️", "⛈️"]
+const dangerousObjects = ["🔥", "🦆", "🦇", "🌧️", "🌪️", "❄️", "💩", "🌩️", "⛈️", "⚡"]
 const deadlyObjects = ["💣"]
 const bonusObjects = ["⛽"]
 
@@ -9,14 +9,17 @@ const lifeIndicator = document.querySelector(".life");
 const plane = document.querySelector(".game-window__plane");
 const youWon = document.querySelector(".youWon");
 const youLose = document.querySelector(".youLose");
+const gameStats = document.querySelector(".game-stats");
+const gameStart = document.querySelector(".start-game");
 
 
 const planeProperties = plane.getBoundingClientRect()
 let speed = 12;
 let play = true;
-let life = 100; //zacągnąć punkty z loc. storage z 1 etapu
+let start = false;
+let life = parseInt(localStorage.getItem("myScore")) || 100;
 let points = 0;
-let left = Math.floor((gameWindow.width - plane.getBoundingClientRect().width) / 2);
+let left = Math.round((gameWindow.width - plane.getBoundingClientRect().width) / 2);
 let score = 45
 let distance = 0
 
@@ -25,32 +28,36 @@ function timer(callback, value) {
     value = value || 45;
     let timer = setInterval(function() {
         callback(value);
-        if (value-- <= 0 || !(play)) {
+        if (!start) {
+            return;
+        } else if (value-- <= 0) {
             setTimeout(gameEnd, 500);
             clearInterval(timer);
         }
     }, 1000);
+
 };
 
 new timer(function(value) {
     let timerMsg = "00:" + (value >= 10 ? value : "0" + value);
     time.textContent = timerMsg;
     pointIndicator.innerText = points;
-    points = Math.floor(life * distance * 0.0001);
+    lifeIndicator.innerText = life;
+    points = Math.floor(life * distance * 0.00005);
     score = value;
 });
 
 function gameOver() {
-    console.log("game over");
     play = false;
     youLose.style.visibility = "visible";
+    gameStats.style.visibility = "hidden";
 }
 
 function gameEnd() {
     play = false;
     document.querySelector(".score").innerText = points;
     youWon.style.visibility = "visible";
-
+    gameStats.style.visibility = "hidden";
 
 }
 
@@ -62,12 +69,10 @@ function calculateDistance() {
 function lifeLeft() {
     if (life > 0) {
         life -= 2
-        lifeIndicator.innerText = life;
     } else if (life <= 0) {
         gameOver()
     }
 }
-
 
 class Obstacle {
     constructor(emoji) {
@@ -75,6 +80,7 @@ class Obstacle {
         this.position;
         this.positonY;
         this.HTMLtag;
+
     }
     generateObstacle() {
         this.emoji = dangerousObjects[Math.ceil(Math.random() * dangerousObjects.length - 1)];
@@ -88,12 +94,15 @@ class Obstacle {
     }
     moveObstacle() {
         let num = 0;
+        let objPositionTop
         const x = this.HTMLtag;
         const y = this.positon;
+        this.positonY = objPositionTop
         const interval = setInterval(function() {
             if (play) {
                 num++;
-                x.style.top = `${(num * speed)+gameWindow.top}px`;
+                objPositionTop = `${(num * speed)+gameWindow.top}px`;
+                x.style.top = objPositionTop
                 manageCollision()
                 calculateDistance()
                 if ((x.getBoundingClientRect().top) > gameWindow.height) {
@@ -103,8 +112,8 @@ class Obstacle {
         }, 100);
 
         function stopMoving() {
-            clearInterval(interval)
-            x.style.display = "none";
+            clearInterval(interval);
+            x.remove()
         }
 
         const objPosiitionLeft = parseInt(this.HTMLtag.style.left.replace("px", ""));
@@ -113,17 +122,16 @@ class Obstacle {
 
             const planePosiotionLeft = parseInt(plane.style.left.replace("px", ""))
 
-            if (planePosiotionLeft >= (objPosiitionLeft - 65) &&
-                planePosiotionLeft <= (objPosiitionLeft + 65) &&
-                (num * speed) > 425 &&
-                (num * speed) < 510) {
-                console.log("collision");
+            if (planePosiotionLeft >= (objPosiitionLeft - 55) &&
+                planePosiotionLeft <= (objPosiitionLeft + 45) &&
+                (num * speed) > 435 &&
+                (num * speed) < 500) {
                 lifeLeft();
                 plane.animate([
                     { background: 'none' },
                     { background: 'radial-gradient(circle, rgba(252,0,0,1) 0%, rgba(121,9,9,0) 70%, rgba(255,0,0,0) 100%)' }
                 ], {
-                    duration: 250,
+                    duration: 350,
                     iterations: 1
                 });
             }
@@ -136,7 +144,8 @@ class Obstacle {
 
 for (let i = 0; i < 3; i++) {
     setInterval(function() {
-        if (play) {
+        if (play && start) {
+            gameStart.style.visibility = "hidden"
             const obj = new Obstacle();
             obj.generateObstacle()
             obj.moveObstacle()
@@ -146,10 +155,12 @@ for (let i = 0; i < 3; i++) {
 
 addEventListener("keydown", function(event) {
     if (event.keyCode === 37 && left > 1) {
+        start = true
         left -= 15;
         plane.style.left = `${left + gameWindow.x}px`;
 
     } else if (event.keyCode === 39 && left < (gameWindow.width - planeProperties.width)) {
+        start = true
         left += 15;
         plane.style.left = `${left + gameWindow.x}px`;
     } else if (event.keyCode === 38 && speed < 17) {
