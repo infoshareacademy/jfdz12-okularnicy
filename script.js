9b79902ddee66c368ebe42be6164f0d46b462acd
Helena Makrenek <hmakrenek@gmail.com>
2019-11-21 22:38:19 +0100

    FZ12OK-9 some extra code erased

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ $(function () {
   });
 });
 
-$('.nav-link').click(function () {
-  $('.nav-link').removeClass('active');
-  $(this).addClass('active');
-})
+// $('.nav-link').click(function () {
+//   $('.nav-link').removeClass('active');
+//   $(this).addClass('active');
+// })
 
 //nav-hightlights
 
